6ad3940954ce4bb087594aa0c173f64a703d201d
Derek <derekr@me.com>
2015-02-07 03:18:57 -0800

    Add autoprefixer middleware to fix safari bugs

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(require('express-autoprefixer')());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
